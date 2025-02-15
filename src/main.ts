669e72ae005de648efcadb12ae2472b8b86cf81b
erfan <erfan.wtf44@gmail.com>
2025-02-15 07:37:44 -0500

    chore: Adding Versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // set validation Pipe
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
+  app.setGlobalPrefix("v1");
+  // added version
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: "v1"
+  })
   const port = process.env.PORT ?? 3000;
   await app.listen(port, () => {
     console.log(`server run on http://localhost:${port}`)
