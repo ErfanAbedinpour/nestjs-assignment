fc7e73c512e20e4c401d6a12ea7ce405f9e19f16
erfan <erfan.wtf44@gmail.com>
2025-02-15 08:54:54 -0500

    chore: Updating .env add Error Messages

--- src/responses/error.response.ts
+++ src/responses/error.response.ts
@@ -1,3 +1,5 @@
 export class ErrorMessages {
     static readonly INVALID_INFORMATION = "username or email or phone in exsist before"
+    static readonly UNKNOWS_ERROR = "something went wrong"
+    static readonly USER_NOTFOUND = "invalid credential "
 }
--- src/types/env.interface.ts
+++ src/types/env.interface.ts
@@ -4,5 +4,9 @@ export interface IEnv {
     DB_USER: string;
     DB_PASSWORD: string;
     DB_NAME: string;
-    PORT: string
+    PORT: string,
+    ACCESS_TOKEN_KEY: string
+    ACCESS_EXPIRE: string
+    REFRESH_TOKEN_KEY: string
+    REFRESH_EXPIRE: string
 }
