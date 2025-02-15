fa107905a9c5d1e82a11bffadd83023490831f7a
erfan <erfan.wtf44@gmail.com>
2025-02-15 11:08:35 -0500

    chore: Added Invalid Header and Unauth Error Message

--- src/responses/error.response.ts
+++ src/responses/error.response.ts
@@ -1,5 +1,7 @@
 export class ErrorMessages {
     static readonly INVALID_INFORMATION = "username or email or phone in exsist before"
     static readonly UNKNOWS_ERROR = "something went wrong"
-    static readonly USER_NOTFOUND = "invalid credential "
+    static readonly USER_NOTFOUND = "invalid credential"
+    static readonly INVALID_TOKEN = "token is invalid."
+
 }
