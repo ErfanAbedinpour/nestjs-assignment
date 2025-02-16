b6e4c3e3ebbb61b89107a08230be68b727337557
erfan <erfan.wtf44@gmail.com>
2025-02-16 04:13:01 -0500

    chore: Import UtilModule

--- src/imports/internal.import.ts
+++ src/imports/internal.import.ts
@@ -1,5 +1,6 @@
 import { AuthModule } from "../modules/auth/auth.module";
 import { TaskModule } from "../modules/task/task.module";
 import { UserModule } from "../modules/user/user.module";
+import { UtilModule } from "../modules/utils/util.module";
 
-export const internalImports = [AuthModule, UserModule, TaskModule]
+export const internalImports = [AuthModule, UserModule, TaskModule, UtilModule]
