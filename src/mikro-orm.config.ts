8e390800ff2a9eb3af96dc6d9e5d8674192b4edb
erfan <erfan.wtf44@gmail.com>
2025-02-17 12:32:12 -0500

    chore: Adding Seeder

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from "@mikro-orm/core";
 import { Migrator } from "@mikro-orm/migrations";
 import { MySqlDriver } from "@mikro-orm/mysql";
+import { SeedManager } from "@mikro-orm/seeder";
 import { Logger } from "@nestjs/common";
+import { DatabaseSeeder } from "./seeder/seed";
 
 
 const logger = new Logger("MikroOrm")
 
 const DB_URI = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`
 
+console.log('db uri is', DB_URI)
+
 export default defineConfig({
     clientUrl: DB_URI,
     entities: ['./dist/entities/*.entity.js'],
     port: +process.env.DB_PORT,
-    extensions: [Migrator],
+    extensions: [Migrator, SeedManager],
     dbName: process.env.DB_NAME,
     debug: true,
     baseDir: process.cwd(),
@@ -29,6 +33,12 @@ export default defineConfig({
         disableForeignKeys: true,
         allOrNothing: true,
         dropTables: true,
+        emit: 'ts',
+    },
+    seeder: {
+        path: "./src/seeder",
+        defaultSeeder: DatabaseSeeder.name,
+        glob: '!(*.d).{js,ts}',
         emit: 'ts',
     },
 });
